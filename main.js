0f72f4e51e041f936dcf880b9776fdc07af32add
Mohit248 <83233932+Mohit248@users.noreply.github.com>
2022-01-12 20:39:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+noseX = 0
+noseY = 0
+
+function preload(){
+clown_nose = loadImage("https://i.postimg.cc/JnLVKfcH/580b57fbd9996e24bc43bed5.png");
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    PoseNet = ml5.poseNet(video,modelLoaded);
+    PoseNet.on('pose',gotPoses);
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+    image(clown_nose,noseX,noseY,30,30)
+}
+
+function take_Snapshot(){
+    save('my_Filter_Image.png');
+}
+
+function modelLoaded(){
+    console.log("PoseNet is Intiated");
+}
+
+function gotPoses(results){
+  if (results.length > 0) {
+      console.log(results);
+      noseX = results[0].pose.nose.x-15;
+      noseY = results[0].pose.nose.y-15;
+      console.log("nose x ="+noseX);
+      console.log("nose y ="+noseY);
+  }
+}
